fef883d90fa3533998c005cba78ba9d30af872a0
ToshkaTab <tabunov900@gmail.com>
2018-11-26 22:19:29 +0200

    Applied HashSet

--- Practice8/src/com/fit/Main.java
+++ Practice8/src/com/fit/Main.java
@@ -1,14 +1,18 @@
 package com.fit;
 
-import java.awt.*;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        HashSet<POJO> set = init();
+        System.out.println("\nHashSet:");
+        display(set);
+        System.out.println("___");
+        System.out.println("Result: ");
+        display(getElements(set));
+    }
+    public static HashSet<POJO> init(){
         HashSet<POJO> set = new HashSet<>();
 
         System.out.println(set.add(new POJO("P0", "t0", 0)));
@@ -18,20 +22,13 @@ public class Main {
         System.out.println(set.add(new POJO("P3", "t3", 2)));
         System.out.println(set.add(new POJO("P3", "t3", 3)));
 
-        System.out.println("\nHashSet:");
+        return set;
+    }
+    public static void display(HashSet<POJO> set){
         for(POJO element : set){
             System.out.println(element);
         }
-        System.out.println("___");
-
-        HashSet<POJO> uniqueElements = getElements(set);
-
-        System.out.println("Result: ");
-        for(POJO element : uniqueElements){
-            System.out.println(element);
-        }
     }
-
     public static HashSet<POJO> getElements(HashSet<POJO> set){
         HashSet<POJO> uniqueElements = new HashSet<POJO>();
         HashSet<POJO> forDelete;
